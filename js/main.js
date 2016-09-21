6c995cdc9dc2c055503849ee68801c7e3e5d70a7
Raniro Coelho <errecme@gmail.com>
2016-09-21 17:13:45 -0300

    add temperature switch btn

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 $(document).ready(function(){
   var latitude, longitude, cityNameG;
-  var temperature, weatherDescription, cityNameW;
-  var openweathermapURL;
+  var temperature, weatherDescription, cityNameW, sunriseTime, sunsetTime;
+  var currentTime = new Date().getTime() / 1000;
+  var DayNight;
 
   $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
     latitude = data.lat;
     longitude = data.lon;
     cityNameG = data.city;
 
-    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + " &appid=5f7bcf238dc7056a7325948af9cb61be", function(data) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=6521fda1207eae043017412fa964c906", function(data) {
         temperature = data.main.temp; // K
         weatherDescription = data.weather[0].description;
         cityNameW = data.name;
+        sunriseTime = data.sys.sunrise;
+        sunsetTime = data.sys.sunset;
 
-        $(".temperature").text("Temperature: " + temperature + " K");
+        if (currentTime > sunriseTime && currentTime < sunsetTime) {
+          DayNight = "Day";
+        } else { DayNight = "Night";}
+
+        $(".temperature").text("Temperature: " + Math.round(temperature - 273.15) + " °C");
+
+        var showCelsius = true;
+        $('.btn').on('click', function(){
+          if(showCelsius === false) {
+            $(".temperature").text("Temperature: " + Math.round(temperature - 273.15) + " °C");
+            $(".btn").text('°F');
+            showCelsius = true;
+          }
+            else {
+              $(".temperature").text(" Temperature: " + Math.round(temperature * 9 / 5 - 459.67) + " °F");
+                $(".btn").text('°C');
+                showCelsius = false;
+            }
+        });
+
         $(".weather").text("Weather condition: " + weatherDescription);
         $(".coords").text("lat: " + latitude + ", lon: " + longitude);
         $(".cityg").text("City-apiGeo: " + cityNameG);
         $(".cityw").text("City-apiWea: " + cityNameW);
+        $(".day-night").text("current time: " + DayNight);
     });
   });
 });
